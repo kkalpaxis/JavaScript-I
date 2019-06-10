4b8746b49a8a30c4e8f49eaf459a4a2ed8313c0d
Karina Kalpaxis <kalpaxis1@gmail.com>
2019-06-10 16:01:49 -0400

    WIP: Objects Challenge 3 Complete

--- assignments/objects.js
+++ assignments/objects.js
@@ -70,7 +70,11 @@ console.log(Antonietta.gender)
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
+function speak (name) {
+  return "Hello, my name is " + name + "!"
+}
 
+console.log(speak("Kennan"))
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
 
